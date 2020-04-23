97c2338385f8f1fbc36cb4a16dde03f1a11454e3
serkankirman <62117224+serkankirman@users.noreply.github.com>
2020-04-23 00:51:25 -0400

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,15 @@
+package Day01_FirstProgramming;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+
+        System.out.println("i like java");
+    }
+
+
+
+
+
+}
